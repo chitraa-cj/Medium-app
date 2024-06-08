85cbb41e304dffd641cf3781c2c9e4f6f216868c
chitraa-jain <164042739+chitraa-cj@users.noreply.github.com>
2024-06-08 23:28:22 +0530

    signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -63,11 +63,12 @@ app.post('/api/v1/signin', async (c) => {
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env?.DATABASE_URL	,
 	}).$extends(withAccelerate());
-
+  
 	const body = await c.req.json();
 	const user = await prisma.user.findUnique({
 		where: {
 			email: body.email,
+      password: body.password
 		}
 	});
 
